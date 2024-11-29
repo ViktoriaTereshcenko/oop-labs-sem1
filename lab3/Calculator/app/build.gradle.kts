0209c5cdf4aa6b18cc21fb9e5df670140eac92d7
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2024-11-29 21:32:49 +0200

    Update build.gradle.kts

--- lab3/Calculator/app/build.gradle.kts
+++ lab3/Calculator/app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("net.objecthunter:exp4j:0.4.8")
-    implementation("com.google.android.gms:play-services-ads:23.5.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
